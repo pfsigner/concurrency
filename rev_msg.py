bfbbc98efad86404815c3e21f11e6e35d3d26d85
pfsigner <proofshow.signer@gmail.com>
2023-12-29 01:16:48 +0800

    Update rev_msg.py

--- rev_msg.py
+++ rev_msg.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import boto3
 
@@ -8,9 +9,11 @@ aws_region = "us-east-1"
 queue_url = os.environ["QUEUE_URL"]
 
 def main():
+  start_time = time.time()
+  duration_in_seconds = 5 * 60 * 60
   sqs_client = boto3.client("sqs", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
 
-  while True:
+  while time.time() - start_time < duration_in_seconds:
     try:
       response = sqs_client.receive_message(
         QueueUrl=queue_url,
@@ -21,13 +24,13 @@ def main():
       )
 
       if "Messages" in response:
-        message = response["Messages"][0]
-        print(f"Received message: {message['Body']}")
+        for message in response["Messages"]:
+          print(f"Received message: {message['Body']}")
 
-        sqs_client.delete_message(
-            QueueUrl=queue_url,
-            ReceiptHandle=message["ReceiptHandle"]
-        )
+          sqs_client.delete_message(
+              QueueUrl=queue_url,
+              ReceiptHandle=message["ReceiptHandle"]
+          )
     except Exception as e:
       print(str(e))
     
